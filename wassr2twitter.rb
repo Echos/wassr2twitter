6660362c1c9e60c9dc0d73a3095bfc9a91d341c7
えちょ <echos@echo-sp.com>
2009-05-19 16:48:20 +0900

    Twitter mentions取得用情報の追加とそれに伴う変数名の見直し

--- wassr2twitter.rb
+++ wassr2twitter.rb
@@ -23,7 +23,6 @@ twitter_id = '<twitter id>'
 twitter_pw = '<twitter passwd>'
 
 # import ID/PW from ~/.pit
-begin
   require 'rubygems'
   require 'pit'
   wassr = Pit::get( 'wassr', :require => {
@@ -38,17 +37,19 @@ begin
   } )
   twitter_id = twitter['user']
   twitter_pw = twitter['pass']
-rescue LoadError
-end
 
-http_port = '80'
+#Twitterのタイムラインを監視するか？
+twitter2wassr = true
+#Wassrに転送するリプライ元ユーザID
+rep_user_ids = ["Echos"]
+
 
 #==================================
 # 定数
 #==================================
 #wassr TLのXML取得
 wassr_apiUrl_for_TL = 'http://api.wassr.jp/statuses/friends_timeline.xml'
-http_port = '80'
+wassr_http_port = '80'
 wassr_apiParam_for_TL = 'page='
 
 #twitter 投稿用情報
@@ -56,40 +57,87 @@ twitter_post_FQDN = 'twitter.com'
 twitter_post_URL  = '/statuses/update.xml'
 twitter_http_port = '80'
 
+#twitterのmention取得
+twitter_apiUrl_for_rep = 'http://twitter.com/statuses/mentions.xml'
+twitter_apiParam_for_rep = 'sinse_id='
+
 #tinyurl api
 tinyurl_postUrl = 'http://tinyurl.com/api-create.php?url='
 tinyurl_http_port = '80'
 
 
-#XML要素名
-xml_elem_id = ['user_login_id',
-               'areacode',
-               'photo_thumbnail_url',
-               'html',
-               'reply_status_url',
-               'text',
-               'id',
-               'link',
-               'reply_user_login_id',
-               'epoch',
-               'rid',
-               'photo_url',
-               'reply_message',
-               'reply_user_nick',
-               'slurl',
-               'areaname',
-               'user/protected',
-               'user/profile_image_url',
-               'user/screen_name']
+#WassrXML要素名（friends_timeline用）
+wassr_xml_elem_id = ['user_login_id',
+                     'areacode',
+                     'photo_thumbnail_url',
+                     'html',
+                     'reply_status_url',
+                     'text',
+                     'id',
+                     'link',
+                     'reply_user_login_id',
+                     'epoch',
+                     'rid',
+                     'photo_url',
+                     'reply_message',
+                     'reply_user_nick',
+                     'slurl',
+                     'areaname',
+                     'user/protected',
+                     'user/profile_image_url',
+                     'user/screen_name']
 
-xml_elem_key = xml_elem_id[6]
-xml_elem_post_name = xml_elem_id[0]
-xml_elem_post_text = xml_elem_id[5]
-xml_elem_post_link = xml_elem_id[7]
-xml_elem_post_rep  = xml_elem_id[4]
+#個々の使用する要素定義（for Wassr）
+wassr_xml_elem_key = wassr_xml_elem_id[6]
+wassr_xml_elem_post_name = wassr_xml_elem_id[0]
+wassr_xml_elem_post_text = wassr_xml_elem_id[5]
+wassr_xml_elem_post_link = wassr_xml_elem_id[7]
+wassr_xml_elem_post_rep  = wassr_xml_elem_id[4]
+
+#TwitterXML要素名（mentions用）
+twitter_xml_elem_id = ['create_at',
+                       'id',
+                       'text',
+                       'source',
+                       'truncated',
+                       'in_reply_to_status_id',
+                       'in_reply_to_user_id',
+                       'favorited',
+                       'in_reply_to_screen_name',
+                       'user/id',
+                       'user/name',
+                       'user/screen_name',
+                       'user/location',
+                       'user/description',
+                       'user/profile_image_url',
+                       'user/url',
+                       'user/protected',
+                       'user/followers_count',
+                       'user/profile_background_color',
+                       'profile_text_color',
+                       'profile_link_color',
+                       'rofile_sidebar_fill_color',
+                       'rofile_sidebar_border_colopr',
+                       'riends_count',
+                       'reated_at',
+                       'avourites_count',
+                       'tc_offset',
+                       'ime_zone',
+                       'profile_background_image_url',
+                       'profile_background_tile',
+                       'statuses_count',
+                       'notifications',
+                       'following']
+
+#TwitterXMLの各発言要素ID
+twitter_xml_root_id = 'statuses/status'
+twitter_xml_elem_post_text = twitter_xml_elem_id[2]
+
+#個々の使用する要素定義（for Twitter）
 
 #ID保存ファイル
-id_file_name = '.wassr_id'
+id_file_name_wassr = '.wassr_id'
+id_file_name_twitter = '.twitter_id'
 
 #==================================
 # 変数
@@ -104,7 +152,7 @@ proxy_scheme, proxy_host, proxy_port =
 #==================================
 #読込済みIDを取得する
 begin
-  tmp_file = open(id_file_name)
+  tmp_file = open(id_file_name_wassr)
   id = tmp_file.gets.to_s.strip
   tmp_file.close
 rescue Errno::ENOENT
@@ -129,10 +177,10 @@ for count in 1..wassr_get_pages do
   xmldoc.elements.each('statuses/status') do |sts|
     #連想配列に格納後、IDをキーに連想配列に格納
     tmpHash = Hash::new
-    xml_elem_id.each do |elm|
+    wassr_xml_elem_id.each do |elm|
       tmpHash[elm] = sts.elements[elm].text.to_s
     end
-    statuses_hash[tmpHash[xml_elem_key]]=tmpHash
+    statuses_hash[tmpHash[wassr_xml_elem_key]]=tmpHash
   end
 end
 
@@ -143,9 +191,9 @@ statuses_hash.sort{|a,b|
   p key
   if id < key.to_i then
     id = key.to_i
-    tmp_name = statuses_hash[key][xml_elem_post_name]
-    tmp_text = statuses_hash[key][xml_elem_post_text]
-    tmp_link = statuses_hash[key][xml_elem_post_link]
+    tmp_name = statuses_hash[key][wassr_xml_elem_post_name]
+    tmp_text = statuses_hash[key][wassr_xml_elem_post_text]
+    tmp_link = statuses_hash[key][wassr_xml_elem_post_link]
     tmp_link = open(tinyurl_postUrl + tmp_link.to_s).read.to_s
 
     #投稿
@@ -162,7 +210,7 @@ statuses_hash.sort{|a,b|
 }
 
 #最終IDを書き込む
-tmp_file = open(id_file_name,'w')
+tmp_file = open(id_file_name_wassr,'w')
 tmp_file.puts id
 tmp_file.close
 
